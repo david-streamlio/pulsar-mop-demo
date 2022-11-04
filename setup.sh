9e3dc58f2c8c081e1cdb4b709bd83b67b680bd7c
david-streamlio <35466513+david-streamlio@users.noreply.github.com>
2022-11-04 12:44:51 -0700

    Fixed some configuration issues

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 docker build ./docker -t apachepulsar/pulsar-mop
 
 ################################################
-# Start AoP enabled Pulsar in standalone mode
+# Start MoP enabled Pulsar in standalone mode
 ################################################
 docker run --name pulsar -dt -p 1883:1883  -p 6650:6650  -p 8080:8080 \
   --mount source=pulsardata,target=/pulsar/data \
